9a0e78a23f26d34d97e536c1adfce459ba56dfaf
windyakin <windyakin@pepabo.com>
2022-05-11 14:32:26 +0900

    Fix command

--- download_kustomize.sh
+++ download_kustomize.sh
@@ -7,8 +7,7 @@ cat "$(dirname $0)/download_urls.txt" | while read -r line; do
   url=$(echo "$line" | awk -F " " '{ print $2 }')
   echo "Download ${version}"
   mkdir -p "/kustomize/${version}"
-  curl -
-  SL "${url}" -o "/kustomize/${version}/${version}.tar.gz"
+  curl -SL "${url}" -o "/kustomize/${version}/${version}.tar.gz"
   tar xzf "/kustomize/${version}/${version}.tar.gz" -C "/kustomize/${version}/"
   rm "/kustomize/${version}/${version}.tar.gz"
 done
